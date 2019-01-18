5fb8c613eb8e27192e14802f2848dbe0228075d8
csPython <cspython@AD25TF04NGF1J-64.local>
2019-01-18 13:31:02 -0500

    Data File Created and Imports Data, Finished Code

---
+++ quizDataSG.py
@@ -0,0 +1,102 @@
+
+#Identify Variables
+q1a = int()
+q1choice = False
+q1t = ("""Which one of these colours are the primary colours?
+1.Yellow
+2.Green
+3.Orange
+4.Purple
+Answer:  """)
+q1c = 1
+
+q2a = int()
+q2choice = False
+q2t = ("""What colours make pink?
+1.Red + Blue
+2.Purple + Blue
+3.Yellow + Red
+4.Red + White
+Answer:  """)
+q2c = 4
+
+q3a = int()
+q3choice = False
+q3t = ("""What colours make the American Flag?
+1.Red + Black + White
+2.Purple + Blue + Green
+3.Yellow + Red + Green
+4.Red + White + Blue
+Answer:  """)
+q3c = 4
+
+q4a = int()
+q4choice = False
+q4t = ("""What colour is blood before oxidation?
+1.Red 
+2.Blue
+3.Purple
+4.Green
+Answer:  """)
+q4c = 1
+
+q5a = int()
+q5choice = False
+q5t = ("""There's a red house to the left and a blue house to the right. Where's the White House?
+1.In The Middle
+2.Behind Them
+3.D.C.
+4.That Makes No Sense
+Answer:  """)
+q5c = 3
+
+q6a = int()
+q6choice = False
+q6t = ("""What colour is not originally on the rainbow?
+1.Red 
+2.Blue
+3.Pink
+4.Green
+Answer:  """)
+q6c = 3
+
+q7a = int()
+q7choice = False
+q7t = ("""You're driving a bus. There are 12 cows with 7 legs, 3 mice with blue eyes, green dogs with one ear, and a cat.
+What colour is the bus driver's eyes? 
+1.Blue
+2.Green
+3.Hazel
+4.Your eye colour
+Answer:  """)
+q7c = 4
+
+q8a = int()
+q8choice = False
+q8t = ("""What colour is on the UA Maker Logo?
+1.Wait, we have a logo?
+2.Purple 
+3.Red
+4.Brown
+Answer:  """)
+q8c = 2
+
+q9a = int()
+q9choice = False
+q9t = ("""What colour is Shandrika's favorite?
+1.Blue
+2.Purple
+3.Yellow
+4.ALL OF THE ABOVE
+Answer:  """)
+q9c = 3
+
+q10a = int()
+q10choice = False
+q10t = ("""How many colours does it take to make a blind man confused?
+1.Just One
+2.All of them
+3.None, He can't see
+4.This makes no sense
+Answer:  """)
+q10c = 3
