da8f63034bef0ff188bf0ea0a75af1ad2cd759a1
csPython <cspython@AD25TF04NGF1J-64.local>
2019-01-18 09:29:16 -0500

    Fifth Commit: Two Questions, Global Variable, Working Score

--- quizSG.py
+++ quizSG.py
@@ -1,12 +1,14 @@
-#Custom Function Added, Score Commented Out
+#Global Variable
 
-#Custom fucntion
+grade = 0
+#Custom function
 def run_quest(quest,check,ansU,ansR):
     while check == False:
         try:
             ansU = int(input(quest))
             if ansU == ansR:
-                #score += 1
+                global grade
+                grade += 1
                 check = True
             elif 0 < ansU < 5:
                 check = True
@@ -14,7 +16,7 @@ def run_quest(quest,check,ansU,ansR):
                 print("Please type a whole number between 1 and 4. ")
         except ValueError:
             print("Seriously? Please put a WHOLE POSITIVE INTEGER between 1 and 4")
-            
+
 
 #Identify Variables
 q1a = int()
@@ -40,3 +42,7 @@ q2c = 4
 #Run Program
 run_quest(q1t, q1choice, q1a, q1c)
 run_quest(q2t, q2choice, q2a, q2c)
+
+#User's Score
+print("Congrats! You got",grade * 50,"% right!")
+            
